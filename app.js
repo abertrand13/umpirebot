e743df657985c61f264f7a73361c31b0800af4bc
Alex Bertrand <alex.bertrand13@gmail.com>
2017-04-07 11:56:09 -0700

    Major changes to app.js

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
 var extend = require('extend'); // port of jquery's extend for deep copy
+var request = require('request');
 
 var pg = require('pg');
 pg.defaults.ssl = true;
@@ -28,11 +29,86 @@ app.post("/groupme", function(req, res) {
     var msg = req.body.text;
     var strikeReg = /.*strike.*/gi;
 
-    if(strikeReg.regex.test(msg)) {
+    if(strikeReg.test(msg)) {
         // someone said strike
         // var nameReg = /.*@
+        
+        // query for members of group 
+        var options = extend(true, {}, optionsTemplate);
+        // options.form.bot_id = botKey;
+        options.method = 'GET';
+        options.url = "https://api.groupme.com/v3/groups/" + req.body.group_id + "?token=" + process.env.ACCESS_TOKEN;
+        
+        request(options, function(error, response, body) {
+            var members = JSON.parse(body).response.members;
+            if(!members) {
+                // error handling here
+            }
+            for(var i = 0; i < members.length; i++) {
+                if(msg.includes(members[i].nickname)) {
+                    // update database accordingly
+                    var name = members[i].nickname;
+                    var userid = members[i].user_id;
+                    var id = members[i].id; // for booting
+                    
+                    pg.connect(process.env.DATABASE_URL, function(err, client) {
+                        if(err) throw err;
+
+                        client.query("INSERT INTO strikes (userid, strikes) values ('" + 
+                                userid +
+                                "', 1) ON CONFLICT (userid) DO UPDATE SET strikes=strikes.strikes+1 WHERE strikes.userid='" +
+                                userid + 
+                                "' RETURNING strikes.strikes",
+                        function(err, res) {
+                            var strikes = res.rows[0].strikes;
+                            var strikesReal = strikes % 3 == 0 ? 3 : strikes % 3;
+
+                            sendMessage("STEERIKE " + strikesReal + " for " + name + "! " + (strikesReal == 3 ? "YER OUT!!" : ""));
+                            if(strikesReal == 3) {
+                                // this means 3 because mod
+                                // boot user from group
+                                bootMember(req.body.group_id, id);
+                            }
+                        });
+                    });
+                    
+                }
+            }
+        });
     }
+    
+    res.send('Message received');
+});
+
+function bootMember(groupid, id) {
+    var options = extend(true, {}, optionsTemplate);
+    options.url = "https://api.groupme.com/v3/groups/" + groupid + "/members/" + id + "/remove?token=" + process.env.ACCESS_TOKEN;
+    request(options);
 }
+
+function sendMessage(msg) {
+    var options = extend(true, {}, optionsTemplate);
+    options.form.bot_id = botKey;
+    options.form.text = msg;
+    request(options);
+}
+
+var headersTemplate = {
+	'Content-Type' : 'application/json'	
+};
+
+var optionsTemplate = {
+	url		: 'https://api.groupme.com/v3/bots/post',
+	method	: 'POST',
+	headers : headersTemplate,
+	form : {}
+};
+
+
+
+app.listen(app.get('port'), function() {
+	console.log("Example app listening on port " + app.get('port') + "!");
+});
 
 // how are you going to do multi-word names?
 // this is only for kicking out so you can probably chug ahead and see what happens
